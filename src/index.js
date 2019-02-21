5aac833c16dbb3d02e226fb3e67fcc697b2217b8
philippbruhin <info@philippbruhin.ch>
2019-02-21 23:51:36 +0100

    updated code from tutorial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { BrowserRouter as Router } from 'react-router-dom'
-import Routes from './routes';
-import Header from './components/Header';
-
-import './index.css';
-
-ReactDOM.render(
-    <Router>
-        <div>
-            <Header />
-            <Routes />
-        </div>
-    </Router>,
-    document.getElementById('root')
-);
+import App from './components/App';
